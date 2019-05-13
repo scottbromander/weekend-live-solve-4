8dba146738f2bbbc3c46b49cd5dd0dcfb03d9282
Scott Bromander <scottbromander@gmail.com>
2019-05-13 10:24:00 -0500

    adds image and descrition toggle

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 
 class GalleryItem extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clicked: true
+        }
+    }
+
+    clickCell = (event) => {
+        this.setState ({
+            clicked : !this.state.clicked
+        });
+    }
+
     render() {
+        let cellHtml = <img src={this.props.path} />;
+
+        if (this.state.clicked == false) {
+            cellHtml = <p>{this.props.description}</p>;
+        }
+
         return (
             <div>
-                <div className="cell">
-                    <img src={this.props.path} />
+                <div className="cell" onClick={this.clickCell}>
+                    {cellHtml}
                 </div>
-
-                <p>{this.props.description}</p>
             </div>
         )
     }
